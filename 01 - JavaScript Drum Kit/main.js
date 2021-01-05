32dee3a46bb26ad5ab337574b9e0d389ceff1a8c
mteresa-jimenez <mteresa.jn@gmail.com>
2021-01-05 17:00:47 +0100

    Add audio

--- 01 - JavaScript Drum Kit/main.js
+++ 01 - JavaScript Drum Kit/main.js
@@ -1,7 +1,12 @@
 "use strict";
 
 function handleAudio(ev) {
-  const audio = document.querySelector(`audio[key-code="${ev.keyCode}"]`);
+  const audio = document.querySelector(`audio[data-key="${ev.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${ev.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0; // rewind to the start
+  audio.play();
+  key.classList.add("playing");
 }
 
 window.addEventListener("keydown", handleAudio);
